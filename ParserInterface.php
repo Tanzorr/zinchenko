54522f21f860a0c8ef4e5f56b41ae79eab63f978
tanzorr <alexx1984@ukr.net>
2019-06-14 14:14:58 +0300

    Parse Interface

---
+++ ParserInterface.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alexx
+ * Date: 14.06.19
+ * Time: 14:09
+ */
+
+interface  ParserInterface
+{
+    public  function progress(  $url,  $tag):array ;
+}
